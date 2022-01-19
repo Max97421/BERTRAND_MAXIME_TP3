a080dcde44624514fd3f9b045666f6b00773b64c
Max97421 <95446273+Max97421@users.noreply.github.com>
2022-01-19 23:07:56 +0100

    Add files via upload

---
+++ TP3_maxime_bertrand/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using global::System.Linq;
+using global::System.Security.Policy;
+using global::TP3_maxime_bertrand;
+using TP3;
+
+namespace TP3_maxime_bertrand
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var listmovie = new global::TP3.MovieCollection().Movies;
+
+            var firstmovie = listmovie.OrderBy(x=>x.ReleaseDate).First().Title;
+            Console.WriteLine(firstmovie);
+
+            var countmovie = listmovie.Count();
+            Console.WriteLine(countmovie);
+
+            var countwithe = listmovie.Count(x=>x.Title.Contains('e'));
+            Console.WriteLine(countwithe);
+
+            var highbudget = listmovie.OrderBy(x => x.Budget).Last().Title;
+            Console.WriteLine(highbudget);
+            
+            var lowbox = listmovie.OrderBy(x => x.BoxOffice).First().Title;
+            Console.WriteLine(lowbox);
+            
+            var ordermovies = listmovie.OrderBy(x => x.Title).Take(11);
+            Console.WriteLine(ordermovies);
+
+            //var counteightees = listmovie.Count(x => x.ReleaseDate) < 1980;
+            //Console.WriteLine(counteightees);
+
+            var exo2 = new Exercice2();
+            exo2.launch();
+
+        }
+    }
+}
---
+++ TP3_maxime_bertrand/Properties/Exercice2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+
+namespace TP3
+{
+    public class Exercice2
+    {
+            Mutex mut = new Mutex();
+            public void fct(string x, int time, int timer)
+            {
+                while (timer != 0)
+                {
+                    mut.WaitOne();
+                    Console.Write(x);
+                    mut.ReleaseMutex();
+                    Thread.Sleep(time);
+                    timer -= 1;
+                
+                }
+            
+            }
+
+
+            public void launch()
+            {
+                var t1 = new Thread( ()=> fct(" ", 50,200));
+                var t2 = new Thread( ()=> fct("*", 40,275));
+                var t3 = new Thread( ()=> fct("°", 20,450));
+            
+                t1.Start();
+                t2.Start();
+                t3.Start();
+
+                t1.Join();
+                t2.Join();
+                t3.Join();
+            }
+        
+        
+    }
+}
